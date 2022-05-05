027f4131db8039bb03f24090c6492934bb3f97f0
Wijnand Karsens <info@codefromanywhere.com>
2022-05-05 21:58:01 +0200

    Improvements

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -17,13 +17,11 @@ const Home: NextPage = () => {
           elegant framework that lets others Develop and Market monetised
           software.
         </h1>
-
         <iframe
           allow="camera; microphone; fullscreen; display-capture; autoplay"
           src="https://meet.jit.si/codefromanywhere"
           style={{ width: "100%", height: "60vh", border: 0 }}
         ></iframe>
-
         <a href="https://meet.jit.si/codefromanywhere">
           {/* lets add an input field on top where you can fill in your name,
            and when clicking to go to jitsi, it also calls a little endpoint 
@@ -52,6 +50,18 @@ const Home: NextPage = () => {
             }}
           >
             Sensible
+          </button>
+        </a>
+        <a href="https://www.youtube.com/channel/UCDw844XEAwsJLkftu9pq2vA">
+          <button
+            style={{
+              backgroundColor: "lightgreen",
+              padding: 15,
+              width: 300,
+              borderRadius: 10,
+            }}
+          >
+            Livestream
           </button>
         </a>
       </main>
