a5e735c5ece9c9652ab0dbaefd7c5465ec14ee82
Wijnand Karsens <info@codefromanywhere.com>
2022-05-05 21:21:36 +0200

    Improvements

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -18,9 +18,28 @@ const Home: NextPage = () => {
           software.
         </h1>
         <a href="https://meet.jit.si/codefromanywhere">
-        <button style={{backgroundColor:"lightblue",padding:15,width:300,borderRadius:10}}>
-          Hang out
-        </button></a>
+          {/* lets add an input field on top where you can fill in your name,
+           and when clicking to go to jitsi, it also calls a little endpoint 
+           that gives anyone with the cfa app that wants to be notified a push 
+           notification "{name} joined the hangout!" ^^ */}
+
+          <iframe
+            allow="camera; microphone; fullscreen; display-capture; autoplay"
+            src="https://meet.jit.si/codefromanywhere"
+            style="height: 100%; width: 100%; border: 0px;"
+          ></iframe>
+
+          <button
+            style={{
+              backgroundColor: "lightblue",
+              padding: 15,
+              width: 300,
+              borderRadius: 10,
+            }}
+          >
+            Hang out
+          </button>
+        </a>
       </main>
     </div>
   );
